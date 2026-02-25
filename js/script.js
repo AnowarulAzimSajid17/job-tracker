26ead4ba3501a67f0c07b9fe0b1d2da830c46425
AnowarulAzimSajid17 <anowarulazimsajid@gmail.com>
2026-02-24 22:34:46 -0500

    tab toggling functionality added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ let total = document.getElementById('totalCount');
 let interviewCount = document.getElementById('interviewCount');
 let rejectedCount = document.getElementById('rejectedCount');
 
+// All Cards Section
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
 const allCardsSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main');
 
 function calculateCount() {
   total.innerText = allCardsSection.children.length;
@@ -15,3 +21,19 @@ function calculateCount() {
 }
 
 calculateCount();
+
+// Tab Section Toggling Logic
+function toogleStyle(id) {
+  allFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+  interviewFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+  rejectedFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+  allFilterBtn.classList.add('bg-white', 'text-black');
+  interviewFilterBtn.classList.add('bg-white', 'text-black');
+  rejectedFilterBtn.classList.add('bg-white', 'text-black');
+
+  const selected = document.getElementById(id);
+
+  selected.classList.remove('bg-white', 'text-black');
+  selected.classList.add('bg-[#3B82F6]', 'text-white');
+}
